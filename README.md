6d213789d00ddf1e91ce1d311b131b805bb156eb
Phils1993 <phils1993@outlook.com>
2025-03-09 11:43:17 +0100

    fsidste commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Nyhedsbreve
+
+Jeg har gjort mig overvejelser vedr. Millers law og gjort så der kun vises 5 nyhedsbreve og derefter vises
+"vis mere" som henviser til en anden side med alle nyhedsbreve samlet
+
+Siden er meget enkel og har link til 3 forskellige sider: vis mere, about og skolens hjemmeside
+
+Der er de samme farver og logoer der går igen på siderne
+
+Man kan komme retur til forsiden fra about og vis mere. Skolen side åbner en ny fane hvilket er praktisk og
+struktureret
+
+Siderne er reponsive 
+
--- js/nyhedsbrev.js
+++ js/nyhedsbrev.js
@@ -1,12 +1,11 @@
-
 // Nyhedsbreve data (eksempel)
 const nyhedsbreve = [
-    { title: "Nyhedsbrev - September", date: "01-09-2024", link: "../nyhedsbreve/01_Nyhedsbrev_SepOkt_2024.pdf" },
-    { title: "Nyhedsbrev - Oktober", date: "01-10-2024", link: "../nyhedsbreve/02_Nyhedsbrev_OktNov_2024.pdf" },
-    { title: "Nyhedsbrev - November", date: "01-11-2024", link: "../nyhedsbreve/03_Nyhedsbrev_NovDec_2024.pdf" },
-    { title: "Nyhedsbrev - December", date: "01-12-2024", link: "../nyhedsbreve/04_Nyhedsbrev_DecJan_2025.pdf"},
-    { title: "Nyhedsbrev - Januar", date: "01-01-2025", link: "../nyhedsbreve/05_Nyhedsbrev_JanFeb_2025.pdf"},
-    { title: "Nyhedsbrev - Februar", date: "01-02-2025", link: "../nyhedsbreve/06_Nyhedsbrev_FebMar_2025.pdf"}
+    {title: "Nyhedsbrev - September", date: "01-09-2024", link: "../nyhedsbreve/01_Nyhedsbrev_SepOkt_2024.pdf"},
+    {title: "Nyhedsbrev - Oktober", date: "01-10-2024", link: "../nyhedsbreve/02_Nyhedsbrev_OktNov_2024.pdf"},
+    {title: "Nyhedsbrev - November", date: "01-11-2024", link: "../nyhedsbreve/03_Nyhedsbrev_NovDec_2024.pdf"},
+    {title: "Nyhedsbrev - December", date: "01-12-2024", link: "../nyhedsbreve/04_Nyhedsbrev_DecJan_2025.pdf"},
+    {title: "Nyhedsbrev - Januar", date: "01-01-2025", link: "../nyhedsbreve/05_Nyhedsbrev_JanFeb_2025.pdf"},
+    {title: "Nyhedsbrev - Februar", date: "01-02-2025", link: "../nyhedsbreve/06_Nyhedsbrev_FebMar_2025.pdf"}
     // Tilføj flere...
 ];
 
@@ -18,16 +17,16 @@ function populateNyhedsbreve() {
     nyhedsbreve.sort((a, b) => new Date(b.date) - new Date(a.date));
 
     // Tilføj nyhedsbreve til dropdown-menu
-    nyhedsbreve.slice(0, 7).forEach(nyhedsbrev => {
+    nyhedsbreve.slice(0, 5).forEach(nyhedsbrev => {
         const listItem = document.createElement('li');
         listItem.innerHTML = `<a href="${nyhedsbrev.link}" target="_blank">${nyhedsbrev.title}</a>`;
         dropdownMenu.appendChild(listItem);
     });
 
-    // "Vis mere"-knap, hvis der er flere end 7 nyhedsbreve
-    if (nyhedsbreve.length > 7) {
+    // "Vis mere"-knap, hvis der er flere end 5 nyhedsbreve
+    if (nyhedsbreve.length > 5) {
         const visMere = document.createElement('li');
-        visMere.innerHTML = `<a href="alle-nyhedsbreve.html">Vis mere</a>`;
+        visMere.innerHTML = `<a href="../modtagerside/arkiv_nyhedsbreve.html">Vis mere</a>`;
         dropdownMenu.appendChild(visMere);
     }
 }
